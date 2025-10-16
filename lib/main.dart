81862edba6570d8c0e50b88f0aff8048cfd2560e
HassanMadkour <hassan20elmsriy@gmail.com>
2025-10-17 01:26:06 +0300

    upload demo and screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       locale: Locale('ar'),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
